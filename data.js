5bf6478ee9424366db38fa2bcbba18204fc4a769
mihirrathod07 <mihir8716@gmail.com>
2024-02-13 13:22:51 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const data=[
+    {name : 'mihir rathod',email:'mihir@gmailcom',contact:'9999'},
+    {name : 'mihir',email:'mihir@gmailcom'},
+    {name : 'rathod',email:'mihir@gmailcom'}
+]
+
+module.exports=data;
