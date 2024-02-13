2e72b3ccd54906c1cd9a8ff98c4474b141c8ebf9
mihirrathod07 <mihir8716@gmail.com>
2024-02-13 14:12:32 +0530

    third commit

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
-const fs = require('fs');
 
-const input = process.argv;
+// Display file list from folder
+const fs =require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname,'files');
+// for(i=0;i<5;i++)
+// {
+//     fs.writeFileSync(dirPath+"/Hello"+i+".txt","a simple tet file");
+// }
 
-if(input[2]=='add')
-{
-    fs.writeFileSync(input[3],input[4])
-}
-else if(input[2]=='remove')
-{
-    fs.unlinkSync(input[3])
-}
-else
-{
-    console.log("invalid input ")
-}
-
+fs.readdir(dirPath,(err,files)=>{
+    files.forEach((item)=>{
+        console.log("file name is", item)
+    })
+})
 
 
 
+// getting input from command line
+// const fs = require('fs');
 
+// const input = process.argv;
 
-
-
-
+// if(input[2]=='add')
+// {
+//     fs.writeFileSync(input[3],input[4])
+// }
+// else if(input[2]=='remove')
+// {
+//     fs.unlinkSync(input[3])
+// }
+// else
+// {
+//     console.log("invalid input ")
+// }
 
 
 
