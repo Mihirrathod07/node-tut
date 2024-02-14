9b79b2ea3524977409f6687e8088a04ea7dc18e5
mihirrathod07 <mihir8716@gmail.com>
2024-02-14 12:46:57 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
 const express =require('express');
 const app =express();
 
 app.get('',(req,res)=>{
-    res.send('hello, this is home page');
+    console.log("data sent by browser =>>>>  ",req.query.name)
+    res.send("Welcome, " +  req.query.name);
+  
 });
 
 app.get('/about',(req,res)=>{
@@ -11,5 +12,22 @@ app.get('/about',(req,res)=>{
 });
 
 app.listen(8000);
+
+
+
+
+
+// const express =require('express');
+// const app =express();
+
+// app.get('',(req,res)=>{
+//     res.send('hello, this is home page');
+// });
+
+// app.get('/about',(req,res)=>{
+//     res.send('hello, this is about page');
+// });
+
+// app.listen(8000);
 
 
