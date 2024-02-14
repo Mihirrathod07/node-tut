7a0bc2a488e4e0710a5b5ff9e1e2f4a6ed1e300c
mihirrathod07 <mihir8716@gmail.com>
2024-02-14 13:09:24 +0530

    third commit

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+
 const express =require('express');
 const app =express();
 
 app.get('',(req,res)=>{
-    console.log("data sent by browser =>>>>  ",req.query.name)
-    res.send("Welcome, " +  req.query.name);
-  
+    res.send(`
+    <h1>hello, this is home page</h1>
+    <a href ="/about"> Go to About page </a>
+    `);
 });
 
 app.get('/about',(req,res)=>{
-    res.send('hello, this is about page');
+    res.send(`
+    <input type = "text" placeholder=" User Name" value="${req.query.name}" />
+    <button>Click Me</button>
+    <a href ="/"> Go to Home page </a>
+    `);
+});
+
+app.get('/help',(req,res)=>{
+    res.send([{
+        name:'mihir',
+        email:'mihir@gmail.com'
+    },
+    {
+        name:'mihir Rathod',
+        email:'mihirrathod@gmail.com' 
+    }
+    ]);
 });
 
 app.listen(8000);
@@ -17,6 +35,28 @@ app.listen(8000);
 
 
 
+
+
+// Routing params request and response
+// const express =require('express');
+// const app =express();
+
+// app.get('',(req,res)=>{
+//     console.log("data sent by browser =>>>>  ",req.query.name)
+//     res.send("Welcome, " +  req.query.name);
+  
+// });
+
+// app.get('/about',(req,res)=>{
+//     res.send('hello, this is about page');
+// });
+
+// app.listen(8000);
+
+
+
+
+// Create a pages
 // const express =require('express');
 // const app =express();
 
