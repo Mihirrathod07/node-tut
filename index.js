04cd6a4019079e66a409415392c1bffa5e45e431
mihirrathod07 <mihir8716@gmail.com>
2024-02-14 14:21:08 +0530

    add 404 page

--- index.js
+++ index.js
@@ -4,10 +4,48 @@ const path = require('path');
 
 const app =express();
 const publicPath= path.join(__dirname,'public');
-
-app.use(express.static(publicPath));
-
+//app.use(express.static(publicPath));
+app.get('',(_,resp)=>{
+resp.sendFile(`${publicPath}/index.html`)
+})
+app.get('/about',(_,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+    })
+    app.get('/help',(_,resp)=>{
+        resp.sendFile(`${publicPath}/help.html`)
+        })
+        app.get('*',(_,resp)=>{
+            resp.sendFile(`${publicPath}/404.html`)
+            })
 app.listen(8000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express =require('express');
+// const path = require('path');
+
+// const app =express();
+// const publicPath= path.join(__dirname,'public');
+
+// app.use(express.static(publicPath));
+
+// app.listen(8000);
 
 
 
