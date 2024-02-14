ea813b6ff6ea762b203975d557aac411dc07c8cf
mihirrathod07 <mihir8716@gmail.com>
2024-02-14 11:24:22 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-
-// Display file list from folder
+const { dir } = require('console');
 const fs =require('fs');
 const path = require('path');
-const dirPath = path.join(__dirname,'files');
-// for(i=0;i<5;i++)
-// {
-//     fs.writeFileSync(dirPath+"/Hello"+i+".txt","a simple tet file");
-// }
+const dirPath = path.join(__dirname,'crud');
+const filePath= `${dirPath}/apple.txt`;
 
-fs.readdir(dirPath,(err,files)=>{
-    files.forEach((item)=>{
-        console.log("file name is", item)
-    })
-})
+//fs.writeFileSync(filePath,'this is the simple files');
+// fs.readFile(filePath,'utf8',(err,item)=>{
+//     console.log(item)
+// })
+// fs.appendFile(filePath,'and file name is apple.txt',(err)=>{
+//     if(!err) console.log("file is updated")
+// })
+// fs.rename(filePath,`${dirPath}/fruit.txt`,(err)=>{
+//     if(!err) console.log("file name changed")
+// })
+fs.unlinkSync(`${dirPath}/fruit.txt`)
+
+// Display file list from folder
+// const fs =require('fs');
+// const path = require('path');
+// const dirPath = path.join(__dirname,'files');
+//  for(i=0;i<5;i++)
+//  {
+//      fs.writeFileSync(dirPath+"/Hello"+i+".txt","a simple tet file");
+//  }
+
+// fs.readdir(dirPath,(err,files)=>{
+//     files.forEach((item)=>{
+//         console.log("file name is", item)
+//     })
+// })
 
 
 
