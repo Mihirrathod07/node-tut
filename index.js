cb63bfd7b77cdfef150b373e37ef3ac970cbfe60
mihirrathod07 <mihir8716@gmail.com>
2024-02-14 13:37:22 +0530

    creating page

--- index.js
+++ index.js
@@ -1,76 +1,9 @@
 
+const express =require('express');
+const app =express();
+
+app.listen(8000);
 
 
-// render html and json
-// const express =require('express');
-// const app =express();
-
-// app.get('',(req,res)=>{
-//     res.send(`
-//     <h1>hello, this is home page</h1>
-//     <a href ="/about"> Go to About page </a>
-//     `);
-// });
-
-// app.get('/about',(req,res)=>{
-//     res.send(`
-//     <input type = "text" placeholder=" User Name" value="${req.query.name}" />
-//     <button>Click Me</button>
-//     <a href ="/"> Go to Home page </a>
-//     `);
-// });
-
-// app.get('/help',(req,res)=>{
-//     res.send([{
-//         name:'mihir',
-//         email:'mihir@gmail.com'
-//     },
-//     {
-//         name:'mihir Rathod',
-//         email:'mihirrathod@gmail.com' 
-//     }
-//     ]);
-// });
-
-// app.listen(8000);
-
-
-
-
-
-
-
-// Routing params request and response
-// const express =require('express');
-// const app =express();
-
-// app.get('',(req,res)=>{
-//     console.log("data sent by browser =>>>>  ",req.query.name)
-//     res.send("Welcome, " +  req.query.name);
-  
-// });
-
-// app.get('/about',(req,res)=>{
-//     res.send('hello, this is about page');
-// });
-
-// app.listen(8000);
-
-
-
-
-// Create a pages
-// const express =require('express');
-// const app =express();
-
-// app.get('',(req,res)=>{
-//     res.send('hello, this is home page');
-// });
-
-// app.get('/about',(req,res)=>{
-//     res.send('hello, this is about page');
-// });
-
-// app.listen(8000);
 
 
