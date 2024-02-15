3429c62d4658cbbddd7c2f02eafa034352ef8685
mihirrathod07 <mihir8716@gmail.com>
2024-02-15 11:38:42 +0530

    middleware

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const express = require('express');
+const app = express();
+const reqFilter = (req, resp, next) => {
+    if (!req.query.age) {
+        resp.send("Please provide your age")
+    }
+    else if (req.query.age<18) {
+        resp.send("You are under aged")
+    }
+    else {
+        next();
+    }
+}
 
-const express =require('express');
-const path = require('path');
+app.use(reqFilter);
 
+app.get('/', (res, resp) => {
+    resp.send('Welcome to Home page')
+});
 
-app.listen(8000);
+app.get('/users', (res, resp) => {
+    resp.send('Welcome to Users page')
+});
+app.listen(8000)
 
 
 
