60ac8be5674fa3cf8536d8bbd52ca73a15fd7561
mihirrathod07 <mihir8716@gmail.com>
2024-02-15 10:34:02 +0530

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 const app =express();
 const publicPath= path.join(__dirname,'public');
-//app.use(express.static(publicPath));
+
+app.set('view engine','ejs');
+
 app.get('',(_,resp)=>{
 resp.sendFile(`${publicPath}/index.html`)
 })
